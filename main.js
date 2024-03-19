12b5f3e6767b9d4e1216c4282869dfada45c627e
kyoungminSong <162668642+kyoungminSong@users.noreply.github.com>
2024-03-19 19:55:16 +0900

    weather app

---
+++ main.js
@@ -0,0 +1,25 @@
+function update() {
+  //  alert("클릭");
+
+  const cities = ["Seoul", "Tokyo", "New York", "Paris"];
+  const num = parseInt(Math.random() * cities.length);
+
+  document.getElementById("location").innerHTML = cities[num];
+  document.getElementById("degree").innerHTML = parseInt(Math.random() * 20);
+
+  document.getElementById("sat-degree").innerHTML = parseInt(
+    Math.random() * 15
+  );
+
+  document.getElementById("sun-degree").innerHTML = parseInt(
+    Math.random() * 15
+  );
+
+  document.getElementById("mon-degree").innerHTML = parseInt(
+    Math.random() * 15
+  );
+
+  document.getElementById("tue-degree").innerHTML = parseInt(
+    Math.random() * 15
+  );
+}
